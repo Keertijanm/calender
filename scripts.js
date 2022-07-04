b1b6071b1a3fe13b67341ffd712152eb9d30fe32
Keertijanm <81510729+Keertijanm@users.noreply.github.com>
2022-07-04 11:14:15 +0530

    save

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@ let currentMonth = today.getMonth();
 let currentYear = today.getFullYear();
 let selectYear = document.getElementById("year");
 let selectMonth = document.getElementById("month");
-
 let months = [
   "Jan",
   "Feb",
